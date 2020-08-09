017262e606406cde5c44d3dfef1ced15f15f86ba
johnj974 <jjdug@hotmail.com>
2020-08-09 11:00:13 +0000

    initial commit, canvas + paddle render for canvas created

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/*  example on how to draw a square on the canvas
+context.fillStyle = "#f00"  // adding color to the square
+context.fillRect(20, 20, 50, 50);  // start drawing at 20 on the x axis and 20 on the y axis a size 50 x 50
+*/
+
+import Paddle from '/js/paddle.js';
+
+// grabbing the gamescreen element and assigning it to a canvas variable
+let canvas = document.getElementById("gameScreen");
+// this allows me to draw onto the canvas
+let context = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+// this clears the screen each time there is a change made to our canvas
+context.clearRect(0, 0, 800, 600);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+
+paddle.draw(context);
+
+
+
