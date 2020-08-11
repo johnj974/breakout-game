27b3a05aab9a799d9222f8dbb25288c006360283
johnj974 <jjdug@hotmail.com>
2020-08-11 09:34:30 +0000

    bricks added

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Paddle from '/src/paddle.js';
 import InputHandler from '/src/input.js';
 import Ball from '/src/ball.js';
+import Brick from "/src/brick.js";
 
 
 export default class Game
@@ -15,7 +16,13 @@ export default class Game
     {
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
-        this.gameObjects = [this.ball, this.paddle]
+        let bricks = []
+        for (let i = 0; i < 10; i++)
+        {
+            bricks.push(new Brick(this, {x : i*52, y:30}));  
+        }
+        this.gameObjects = [this.ball, this.paddle, ...bricks]
+
         // adding the input handler function here
         new InputHandler(this.paddle);
     }
