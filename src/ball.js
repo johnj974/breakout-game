c316b1e61f3aefcd71157d1e4b0577293ab5524b
johnj974 <jjdug@hotmail.com>
2020-08-11 09:09:33 +0000

    added collision logic to ball.js file

--- src/ball.js
+++ src/ball.js
@@ -6,6 +6,7 @@ export default class Ball
         this.size = 16;
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
+        this.game = game;
         this.speed = 
         {
             x : 4,
@@ -26,13 +27,28 @@ export default class Ball
         this.position.x += this.speed.x;
         this.position.y += this.speed.y;
 
+        // if the ball hit wall on left or right of screen
         if(this.position.x + this.size > this.gameWidth || this.position.x < 0)     // this keeps the ball within our canvas on the x axis
         {
             this.speed.x = -this.speed.x;
         }
+
+        // if the ball hits the top or bottom of the screen
         if(this.position.y + this.size > this.gameHeight || this.position.y < 0)     // this keeps the ball within our canvas on the y axis
         {
             this.speed.y = -this.speed.y;
+        }
+
+        // logic for collision between ball and paddle
+        let bottomOfBall = this.position.y + this.size;
+        let topOfPaddle = this.game.paddle.position.y;
+        let leftSideOfPaddle = this.game.paddle.position.x;
+        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
+
+        if (bottomOfBall >= topOfPaddle && this.position.x >= leftSideOfPaddle && this.position.x + this.size <= rightSideOfPaddle)
+        {
+            this.speed.y = -this.speed.y;
+            this.position.y = this.game.paddle.position.y - this.size;
         }
     }
 }
